4ae7928b233192d55ccc8bf79a5d269e9ac192a7
Castro-1 <jecastrog@eafit.edu.co>
2023-01-17 14:02:13 -0500

    start done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Start from "./components/Start";
+import Quiz from "./components/Quiz";
 
 export default function App() {
   const [started, setStarted] = React.useState(false);
+  function startQuiz() {
+    setStarted(true);
+  }
   return (
     <div className="main-container">
-      {started === false && <Start setStarted={setStarted} />}
+      {started === false && <Start startQuiz={startQuiz} />}
+      {started === true && <Quiz />}
     </div>
   );
 }
--- src/components/Start.js
+++ src/components/Start.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Start(props) {
-  return <div className="start">hello world</div>;
+  const { startQuiz } = props;
+  return (
+    <div className="start">
+      <h1 className="start-title">Quizzical</h1>
+      <p className="start-description">Welcome to the most fun trivia quiz!</p>
+      <button className="start-button" onClick={startQuiz}>
+        Start quiz
+      </button>
+    </div>
+  );
 }
